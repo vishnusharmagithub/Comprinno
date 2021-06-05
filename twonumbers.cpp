2358ddc728e6d99e811aed7eac867ad1f49f5f5b
Vishnu sharma <63301008+vishnusharmagithub@users.noreply.github.com>
2021-06-05 11:58:44 +0530

    Add files via upload

---
+++ twonumbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, n;
+        cin >> a >> b >> n;
+        if (n % 2 == 1)
+            a *= 2;
+
+        if (a > b)
+            cout << a / b << endl;
+        else
+            cout << b / a << endl;
+    }
+}
